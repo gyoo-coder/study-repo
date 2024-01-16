3a5039a6caafb0f4bce10aefc40acb028014d8ae
gyoo-coder <gyoogler.lee@gmail.com>
2024-01-16 16:57:59 +0900

    Self git process testing 2
    change number to range 20, if dividable by 15, print "yeardream"

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
-# 과제: 1부터 10까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
+# 과제: 1부터 20까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
 # 나머지 모든 경우는 숫자 그대로 출력
 # 추가 과제: 5의 배수일 때, dream 출력
-for i in range(1, 11):
+# 추가 과제2: 15의 배수일 때, yeardeam 출력
+for i in range(1, 21):
     if i % 3 == 0:
         print("year")
     elif i % 5 == 0:
         print("dream")
+    elif i % 15 == 0:
+        print("yeardream")
